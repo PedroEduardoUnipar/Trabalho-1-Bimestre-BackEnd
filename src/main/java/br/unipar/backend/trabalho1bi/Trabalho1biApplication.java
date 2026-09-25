96280393c0eadb71c9307e8d5493c69c261baa78
PedroEduardoUnipar <pedro110500@edu.unipar.br>
2026-09-25 20:26:40 -0300

    Adiciona classe principal da aplicação

---
+++ src/main/java/br/unipar/backend/trabalho1bi/Trabalho1biApplication.java
@@ -0,0 +1,13 @@
+package br.unipar.backend.trabalho1bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho1biApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trabalho1biApplication.class, args);
+    }
+
+}
