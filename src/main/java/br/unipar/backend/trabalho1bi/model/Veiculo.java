7372a4b36f8fa03472bdd67351cfa3db1a4d2e3d
PedroEduardoUnipar <pedro110500@edu.unipar.br>
2026-09-25 20:27:02 -0300

    Adiciona modelo Veiculo

---
+++ src/main/java/br/unipar/backend/trabalho1bi/model/Veiculo.java
@@ -0,0 +1,17 @@
+package br.unipar.backend.trabalho1bi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Veiculo {
+
+    private Integer id;
+    private String nome;
+    private String marca;
+    private String modelo;
+    private Integer ano;
+
+
+}
