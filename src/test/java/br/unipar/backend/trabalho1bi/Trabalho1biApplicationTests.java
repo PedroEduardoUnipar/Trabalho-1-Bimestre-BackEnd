3d9ede947eb40d7411674b6e96b3aef37818ee28
PedroEduardoUnipar <pedro110500@edu.unipar.br>
2026-09-25 20:27:13 -0300

    Adiciona teste inicial da aplicação

---
+++ src/test/java/br/unipar/backend/trabalho1bi/Trabalho1biApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unipar.backend.trabalho1bi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho1biApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
